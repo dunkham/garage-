465687e7ca1a55e6d4dd95667db9028073a527b4
dunkham <40130287+dunkham@users.noreply.github.com>
2019-08-16 15:21:10 +1000

    reloads the image every 5 seconds

---
+++ www/html/js/image.js
@@ -0,0 +1,7 @@
+var myImageElement = document.getElementById('myImage');
+myImageElement.src = 'screen.jpg?rand=' + Math.random();
+
+setInterval(function() {
+    var myImageElement = document.getElementById('myImage');
+    myImageElement.src = 'screen.jpg?rand=' + Math.random();
+}, 5000);
